c14f0c75cbbb586624751ddb93dc9a63bfcfaae7
bart-kosmala <bartoshmen@gmail.com>
2020-04-15 18:29:28 +0600

    post 15

--- src/zajecia15/Main.java
+++ src/zajecia15/Main.java
@@ -9,7 +9,7 @@ import java.util.ArrayList;
 
 public class Main {
 	public static void main(String[] args) {
-		zadanie1();
+		domowe1();
 	}
 
 	public static void przyklad1() {
@@ -56,12 +56,11 @@ public class Main {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-
 	}
 
 	public static void przyklad3() {
-		String path = "./tekst.txt";
-		String text = "test1\ntest2\n";
+		String path = "./tekst2.txt";
+		String text = "test1\n\ttest2\n";
 
 		try {
 			FileWriter fw = new FileWriter(path);
@@ -86,37 +85,84 @@ public class Main {
 		try {
 			BufferedReader br = new BufferedReader(new FileReader(readPath));
 			String line = "";
-			
+
 			while ((line = br.readLine()) != null) {
 				String[] record = line.split(";");
 				String name = record[0];
 				String number = record[1];
-				
+
 				if (numbers.contains(number)) {
 					names.add(name);
 				}
 			}
-			
+
 			br.close();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
+
 		/* zapis */
 		try {
 			FileWriter fw = new FileWriter(savePath);
-			
+
 			for (String name : names) {
 				fw.write(name + "\n");
 			}
-			
+
 			fw.close();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static void domowe1() {
-		
+		String readPath = "./baza.txt";
+		String savePath = "./domowe.txt";
+
+		String numbersToSearch = "22";
+
+		try {
+			FileReader fr = new FileReader(readPath);
+			FileWriter fw = new FileWriter(savePath);
+
+			int character = 0;
+
+			while (character != -1) {
+				String line = "";
+				boolean isOk = true;
+				boolean isOver = false;
+
+				while ((character = fr.read()) != '\n') {
+					line += (char) character;
+					if (character == -1) {
+						isOver = true;
+						break;
+					}
+				}
+
+				if (!isOver) {
+					String[] record = line.split(";");
+
+					for (int i = 0; i < numbersToSearch.length(); i++) {
+						if (numbersToSearch.charAt(i) != record[1].charAt(i)) {
+							isOk = false;
+							break;
+						}
+					}
+
+					if (isOk) {
+						fw.write("nazwisko: " + record[0] + ", numer: " + record[1] + "\n");
+					}
+				}
+			}
+
+			System.out.println("Zrobione!");
+
+			fr.close();
+			fw.close();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
