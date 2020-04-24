27eec466647f3a965504a0ac4939aff6f226137b
bart-kosmala <bartoshmen@gmail.com>
2020-04-24 20:56:36 +0200

    post 17

--- src/zajecia17/Main.java
+++ src/zajecia17/Main.java
@@ -3,8 +3,6 @@ package zajecia17;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("ok17");
-		
 		new PlayerFrame();
 	}
 
--- src/zajecia17/PlayerFrame.java
+++ src/zajecia17/PlayerFrame.java
@@ -1,5 +1,6 @@
 package zajecia17;
 
+import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -8,55 +9,74 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 
 public class PlayerFrame extends JFrame implements ActionListener {
-	
+	private final int MAX_INDEX = 9;
+	private final int WIDTH = 335;
+	private final int HEIGHT = 105;
+
 	private JButton plus;
 	private JButton minus;
 	private JButton check;
-	
+
 	private JLabel index;
 	private JLabel score;
-	
+
 	private PlayerData playerData;
-	
+	private int currentIndex;
+
 	public PlayerFrame() {
-		super("Sprawdź wynik");
+		super("Check score at index");
+
+		/* ustawienia okna */
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(WIDTH, HEIGHT);
 		setResizable(false);
-		setLayout(null);
 		setVisible(true);
-		setSize(720, 480);
-		
+		setLayout(null);
+
+		/* tworzenie przycisków i pól tekstowych */
 		plus = new JButton("Plus");
 		plus.setBounds(5, 5, 100, 30);
 		plus.addActionListener(this);
-		
+
 		minus = new JButton("Minus");
 		minus.setBounds(5, 40, 100, 30);
 		minus.addActionListener(this);
-		
+
 		check = new JButton("Check");
 		check.setBounds(200, 5, 100, 30);
 		check.addActionListener(this);
-		
+
 		index = new JLabel("0");
 		index.setBounds(110, 5, 100, 30);
-		
+
 		score = new JLabel("none");
 		score.setBounds(305, 5, 100, 30);
-		
-		playerData = new PlayerData();
-		
+
 		add(plus);
 		add(minus);
 		add(check);
-		
+
 		add(index);
 		add(score);
+
+		/* inicjalizacja potrzebnych zmiennych */
+		playerData = new PlayerData();
+		currentIndex = 0;
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
-		
-		//playerData.getValue(0);
+		if (arg0.getSource() == plus && currentIndex != MAX_INDEX) {
+			currentIndex++;
+
+		} else if (arg0.getSource() == minus && currentIndex != 0) {
+			currentIndex--;
+
+		} else if (arg0.getSource() == check) {
+			int result = playerData.getValue(currentIndex);
+			score.setText(String.valueOf(result));
+		}
+
+		index.setText(String.valueOf(currentIndex));
 	}
 }
